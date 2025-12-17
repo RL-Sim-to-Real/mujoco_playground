bd64b0cdeaf0e09eea36b8eefbbb06a672543907
Alireza Azimi <sazimi@ualberta.ca>
2025-12-17 11:14:11 -0700

    save for now

--- mujoco_playground/_src/manipulation/franka_emika_panda/push.py
+++ mujoco_playground/_src/manipulation/franka_emika_panda/push.py
@@ -664,8 +664,8 @@ class PandaPushCuboid(panda.PandaBase):
     dx = strip_half[0] - jp.abs(box_xy[0] - strip_pos[0])
     dy = strip_half[1] - jp.abs(box_xy[1] - strip_pos[1])
     # margin = jp.minimum(dx, dy)  # how far from closest edge (in meters)
-
-    in_bounds = (dx >= 0) & (dy >= 0)
+    box_on_ground = box_pos[2] <0.03
+    in_bounds = (dx >= 0) & (dy >= 0) & box_on_ground
 
     delta = jp.linalg.norm(box_xy - prev_xy)
 
@@ -678,7 +678,7 @@ class PandaPushCuboid(panda.PandaBase):
     center_delta = jp.linalg.norm(box_pos - ee_pos)
     w_center = -0.5
     reward = in_bounds.astype(jp.float32) * (w_move * delta) + (w_center * center_delta)
-
+    reward = jp.clip(reward, -1e3, 1e3)
 
     state.info['prev_box_pos'] = data.xpos[self._obj_body] 
 
